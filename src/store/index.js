98026f9844c047080f5d7806824b1089e9f0887f
Yuri Gonçalves <65895389+yuriv15@users.noreply.github.com>
2021-09-08 00:08:24 -0300

    v1.0

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,51 @@
 import { createStore } from 'vuex'
+import axios from "axios";
 
 export default createStore({
   state: {
+    products: [],
+    productsInBag: [],
   },
   mutations: {
+    loadProducts(state, products) {
+      state.products = products;
+    },
+    addToBag(state, product) {
+      state.productsInBag.push(product);
+      localStorage.setItem("productsInBag", JSON.stringify(state.productsInBag));
+    },
+    removeFromBag(state, productId) {
+      let updatedBag = state.productsInBag.filter(item => productId != item.id);
+      state.productsInBag = updatedBag;
+      localStorage.setItem("productsInBag", JSON.stringify(state.productsInBag));
+    },
+    loadBag(state, products) {
+      state.productsInBag = products;
+    }
   },
   actions: {
+    loadProducts ({ commit }){
+      axios
+          .get('https://fakestoreapi.com/products')
+          .then(response => {
+            commit('loadProducts', response.data);
+          })
+    },
+    addToBag({ commit }, product) {
+      commit('addToBag', product);
+    },
+    removeFromBag({ commit }, productId) {
+      if (confirm('Are you sure you want to remove the item from bag?')) {
+      commit('removeFromBag', productId);
+      }
+    },
+    loadBag ({ commit }){
+      if (localStorage.getItem("productsInBag")) {
+        commit('loadBag', JSON.parse(localStorage.getItem("productsInBag")));
+      }
+    },
   },
+
   modules: {
   }
 })
